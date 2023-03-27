2acf49638cb9e16d04d002a5de41217f7bd1b4b9
Hunter <huntersfred@gmail.com>
2023-03-27 03:18:26 -0400

    feat(Project): Added Power and other things.
    Power decreases your speed, therefor making it easier. If you are too low on power, your are probably going to die.

--- InfiniteRunner/Assets/_Scripts/Player/PlayerController.cs
+++ InfiniteRunner/Assets/_Scripts/Player/PlayerController.cs
@@ -1,18 +1,38 @@
 using System;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     #region Inspector Variables
 
+    [Header("Points")]
+    [SerializeField] private int _points;
+    [SerializeField] private TMP_Text _pointsText;
+
+    [Header("Power Variables")]
+    [SerializeField] private float _powerLossMultiplier;
+    [SerializeField] private float _addPowerAmount = 10;
+
+    [Header("Power Settings")]
+    [SerializeField] private float _power;
+    [SerializeField] private float _maxPower;
+    [SerializeField] private float _speedBonus;
+    [SerializeField] private Slider _powerSlider;
+
     [Header("Movement")]
     [SerializeField] private float _speed;
     [SerializeField] private float _maxFallSpeed;
     [SerializeField] private float _jumpDecay;
     [SerializeField] private float _jumpMultiplier;
+
+    [Header("Misc")]
+    [SerializeField] private bool _isInvincable;
 
     [Header("Other")]
     [SerializeField] private UnityEvent OnJump;
@@ -35,9 +55,33 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.velocity = new Vector2(_speed, 0);
+
+        _powerSlider.maxValue = _maxPower;
+        _powerSlider.minValue = 0;
+
     }
 
-    private void Update() => HandleMovement();
+    private void Update()
+    {
+        HandlePower();
+        HandleMovement();
+    }
+
+    private void HandlePower()
+    {
+        if (_power == 0)
+        {
+            Die();
+            return;
+        }
+
+        _power -= Time.deltaTime * _powerLossMultiplier;
+
+        _powerSlider.value = _power;
+
+
+        _speedBonus = (_maxPower / _power);
+    }
 
     private void HandleMovement()
     {
@@ -51,7 +95,7 @@ public class PlayerController : MonoBehaviour
         float vertical = HandleJump();
 
         // Horizontal
-        _rb.velocity = new (_speed * direction.x, vertical);
+        _rb.velocity = new (_speed * direction.x * _speedBonus, vertical);
     }
 
     private float HandleJump()
@@ -65,6 +109,28 @@ public class PlayerController : MonoBehaviour
         
         return Mathf.Lerp(_rb.velocity.y, _maxFallSpeed, _jumpDecay);
     }
+
+    #region Effects
+
+    public void Die()
+    {
+        if (_isInvincable) return;
+        print("Die");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void AddPower()
+    {
+        _power += _addPowerAmount;
+    }
+    
+    public void AddPoint()
+    {
+        _points++;
+        _pointsText.text = "Points: " + _points.ToString();
+    }
+
+    #endregion
 
     #region Events
 
--- InfiniteRunner/Assets/_Scripts/World/BarSpawner.cs
+++ InfiniteRunner/Assets/_Scripts/World/BarSpawner.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BarSpawner : MonoBehaviour
 {
+    [SerializeField] private float _chanceToSpawn = 100;
+    [SerializeField] private Vector2 _minMaxWidth;
     [SerializeField] private float _barInterval;
     [SerializeField] private float _offscreenAmount;
 
     [SerializeField] private GameObject _barPrefab;
+
+    private List<GameObject> _barList = new ();
 
     private Vector2 _spawnPos;
     private Vector2 _currentPos;
@@ -20,10 +24,23 @@ public class BarSpawner : MonoBehaviour
 
         if (_currentPos.y - _spawnPos.y >= _barInterval)
         {
-            Instantiate(_barPrefab, _currentPos + Vector2.up * _offscreenAmount, Quaternion.identity);
+            if (Random.Range(0, 100) > _chanceToSpawn)
+                return;
+
+            GameObject bar = Instantiate(_barPrefab, _currentPos + Vector2.up * _offscreenAmount, Quaternion.identity);
+            bar.transform.position += Vector3.right * Random.Range(_minMaxWidth.x, _minMaxWidth.y);
+
+            _barList.Add(bar);
+
+            if(_barList.Count > 4)
+            {
+                Destroy(_barList[0]);
+                _barList.RemoveAt(0);
+            }
+
             _spawnPos = _currentPos;
         }
-        
+
     }
 
 }
