bfd49c6abc451c7709712cd5ee3233ab64339feb
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2023-03-30 00:23:10 -0400

    feat(Spawners): Added Random Difficulty Increase
    At the beginning of the game it will spawn easier bars, but at some random number, it will begin spawning harder bars

--- InfiniteRunner/Assets/_Scripts/World/BarSpawner.cs
+++ InfiniteRunner/Assets/_Scripts/World/BarSpawner.cs
@@ -11,13 +11,24 @@ public class BarSpawner : MonoBehaviour
 
     [SerializeField] private GameObject[] _barPrefab;
 
+    [SerializeField] private bool _hasIncreasedDifficulty;
+    [SerializeField] private Vector2 _increaseDifficultyAfter;
+
+    [SerializeField] private GameObject[] _difficultBars;
+
     private List<GameObject> _barList = new ();
 
     private Vector2 _spawnPos;
     private Vector2 _currentPos;
+    private int _spawnCount;
     private float _nextInterval = 10;
+    private float _increaseDifficultyNumber;
 
-    private void Start() => _spawnPos = transform.position;
+    private void Start()
+    {
+        _spawnPos = transform.position;
+        _increaseDifficultyNumber = Random.Range(_increaseDifficultyAfter.x, _increaseDifficultyAfter.y);
+    }
 
     private void FixedUpdate()
     {
@@ -28,9 +39,21 @@ public class BarSpawner : MonoBehaviour
             if (Random.Range(0, 100) > _chanceToSpawn)
                 return;
 
+            GameObject bar;
             Vector2 verticalPosition = _currentPos + Vector2.up * _offscreenAmount;
 
-            GameObject bar = Instantiate(_barPrefab[Random.Range(0, _barPrefab.Length)], verticalPosition, Quaternion.identity);
+            // Increase Difficulty
+            if (_hasIncreasedDifficulty && _spawnCount >= _increaseDifficultyNumber)
+            {
+                bar = Instantiate(_difficultBars[Random.Range(0, _difficultBars.Length)], verticalPosition, Quaternion.identity);
+
+            }
+            else
+            {
+                bar = Instantiate(_barPrefab[Random.Range(0, _barPrefab.Length)], verticalPosition, Quaternion.identity);
+            }
+
+
             bar.transform.position += Vector3.right * Random.Range(_minMaxWidth.x, _minMaxWidth.y);
             bar.transform.position += Vector3.back;
 
@@ -46,6 +69,8 @@ public class BarSpawner : MonoBehaviour
             _nextInterval = Random.Range(_minMaxSpawnInterval.x, _minMaxSpawnInterval.y);
 
             _spawnPos = _currentPos;
+
+            _spawnCount++;
         }
 
     }
